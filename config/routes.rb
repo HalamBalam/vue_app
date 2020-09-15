a5a2143fd715235644b8412985bb96a587d82ae2
anton <antonsurganov@yandex.ru>
2020-09-15 18:28:01 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root 'application#index'
+
+  namespace :admin do
+    devise_for :staffs
+  end
+
+  namespace :user do
+    devise_for :clients
+  end
+  
 end
